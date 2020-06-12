3e587eac2c35943020fcd6f718c9fd5d3a7675f6
calvinhxx <18166855846@163.com>
2020-06-12 09:42:30 +0800

    两种connect 信号形参与槽的差异

--- testSenderReciever/mainwindow.cpp
+++ testSenderReciever/mainwindow.cpp
@@ -7,14 +7,20 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    //信号和槽同形参
-    connect(this, SIGNAL(testSignalInt(int)), this, SLOT(testSlotInt(int)));
-    //信号与槽不同形参 信号有形参，槽无形参
-    connect(this, SIGNAL(testSignalInt(int)), this, SLOT(testSlotVoid()));
-    //信号与槽不同形参 信号无形参，槽需要参数  错误不能使用
-    connect(this, SIGNAL(testSignalVoid()), this, SLOT(testSlotInt(int)));
-//    emit testSignalInt(4);
-    emit testSignalVoid();
+    //qt< 4.8
+//    //信号和槽同形参
+//    connect(this, SIGNAL(testSignalInt(int)), this, SLOT(testSlotInt(int)));
+//    //信号与槽不同形参 信号有形参，槽无形参
+//    connect(this, SIGNAL(testSignalInt(int)), this, SLOT(testSlotVoid()));
+//    //信号与槽不同形参 信号无形参，槽需要参数  运行阶段报错
+//    connect(this, SIGNAL(testSignalVoid()), this, SLOT(testSlotInt(int)));
+    //qt< 5
+//    connect(this, &MainWindow::testSignalInt, this, &MainWindow::testSlotInt);
+//    connect(this, &MainWindow::testSignalInt, this, &MainWindow::testSlotVoid);
+    //编译阶段报错
+//    connect(this, &MainWindow::testSignalVoid, this, &MainWindow::testSlotInt);
+    emit testSignalInt(4);
+//    emit testSignalVoid();
 }
 void MainWindow::testSlotVoid()
 {
