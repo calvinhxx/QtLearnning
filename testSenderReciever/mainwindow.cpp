5abea09f2b695ec4bda6cd8943541a75c77a686f
Gogs <gogs@fake.local>
2020-06-12 09:29:20 +0800

    信号与槽不同形参

---
+++ testSenderReciever/mainwindow.cpp
@@ -0,0 +1,32 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    //信号和槽同形参
+    connect(this, SIGNAL(testSignalInt(int)), this, SLOT(testSlotInt(int)));
+    //信号与槽不同形参 信号有形参，槽无形参
+    connect(this, SIGNAL(testSignalInt(int)), this, SLOT(testSlotVoid()));
+    //信号与槽不同形参 信号无形参，槽需要参数  错误不能使用
+    connect(this, SIGNAL(testSignalVoid()), this, SLOT(testSlotInt(int)));
+//    emit testSignalInt(4);
+    emit testSignalVoid();
+}
+void MainWindow::testSlotVoid()
+{
+    qDebug() << "testSlotVoid";
+}
+void MainWindow::testSlotInt(int value)
+{
+    qDebug() << "testSlot" << value;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
---
+++ testSenderReciever/mainwindow.h
@@ -0,0 +1,27 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+signals:
+    void testSignalVoid();
+    void testSignalInt(int);
+private slots:
+    void testSlotVoid();
+    void testSlotInt(int);
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
